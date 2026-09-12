12a79e63f2b831897672066cf24d427ef25e3370
Michell Larin <michell2026gomez@gmail.com>
2026-09-12 16:13:11 -0600

    Meses acumulados a n meses

--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
@@ -29,15 +29,12 @@
         private void InitializeComponent()
         {
             this.lblCalculo = new System.Windows.Forms.Label();
-            this.txtSueldo = new System.Windows.Forms.TextBox();
-            this.lblSueldo = new System.Windows.Forms.Label();
+            this.txtMontoMensual = new System.Windows.Forms.TextBox();
             this.btnCalcular = new System.Windows.Forms.Button();
-            this.btnLimpiar = new System.Windows.Forms.Button();
-            this.lblISSS = new System.Windows.Forms.Label();
-            this.lblAFP = new System.Windows.Forms.Label();
-            this.lblISR = new System.Windows.Forms.Label();
-            this.lblTotal = new System.Windows.Forms.Label();
-            this.lblSalarioaResibir = new System.Windows.Forms.Label();
+            this.txtMeses = new System.Windows.Forms.NumericUpDown();
+            this.dgvAcumulado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMeses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAcumulado)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCalculo
@@ -46,32 +43,22 @@
             this.lblCalculo.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCalculo.Location = new System.Drawing.Point(77, 25);
             this.lblCalculo.Name = "lblCalculo";
-            this.lblCalculo.Size = new System.Drawing.Size(318, 26);
+            this.lblCalculo.Size = new System.Drawing.Size(365, 26);
             this.lblCalculo.TabIndex = 0;
-            this.lblCalculo.Text = "Calculo  de ley (ISSS, AFP, ISR)";
+            this.lblCalculo.Text = "Tabla de meses acumulados a n meses";
             // 
-            // txtSueldo
+            // txtMontoMensual
             // 
-            this.txtSueldo.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSueldo.Location = new System.Drawing.Point(157, 78);
-            this.txtSueldo.Name = "txtSueldo";
-            this.txtSueldo.Size = new System.Drawing.Size(177, 34);
-            this.txtSueldo.TabIndex = 1;
-            // 
-            // lblSueldo
-            // 
-            this.lblSueldo.AutoSize = true;
-            this.lblSueldo.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSueldo.Location = new System.Drawing.Point(68, 81);
-            this.lblSueldo.Name = "lblSueldo";
-            this.lblSueldo.Size = new System.Drawing.Size(83, 26);
-            this.lblSueldo.TabIndex = 2;
-            this.lblSueldo.Text = "Sueldo:";
+            this.txtMontoMensual.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMontoMensual.Location = new System.Drawing.Point(23, 142);
+            this.txtMontoMensual.Name = "txtMontoMensual";
+            this.txtMontoMensual.Size = new System.Drawing.Size(177, 34);
+            this.txtMontoMensual.TabIndex = 1;
             // 
             // btnCalcular
             // 
             this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(397, 126);
+            this.btnCalcular.Location = new System.Drawing.Point(183, 365);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(149, 95);
             this.btnCalcular.TabIndex = 3;
@@ -79,84 +66,38 @@
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
-            // btnLimpiar
+            // txtMeses
             // 
-            this.btnLimpiar.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(397, 251);
-            this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(149, 93);
-            this.btnLimpiar.TabIndex = 4;
-            this.btnLimpiar.Text = "Limpiar";
-            this.btnLimpiar.UseVisualStyleBackColor = true;
-            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            this.txtMeses.Location = new System.Drawing.Point(23, 87);
+            this.txtMeses.Name = "txtMeses";
+            this.txtMeses.Size = new System.Drawing.Size(120, 22);
+            this.txtMeses.TabIndex = 10;
             // 
-            // lblISSS
+            // dgvAcumulado
             // 
-            this.lblISSS.AutoSize = true;
-            this.lblISSS.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblISSS.Location = new System.Drawing.Point(68, 182);
-            this.lblISSS.Name = "lblISSS";
-            this.lblISSS.Size = new System.Drawing.Size(123, 26);
-            this.lblISSS.TabIndex = 5;
-            this.lblISSS.Text = "ISSS (3%): ";
-            // 
-            // lblAFP
-            // 
-            this.lblAFP.AutoSize = true;
-            this.lblAFP.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAFP.Location = new System.Drawing.Point(68, 219);
-            this.lblAFP.Name = "lblAFP";
-            this.lblAFP.Size = new System.Drawing.Size(139, 26);
-            this.lblAFP.TabIndex = 6;
-            this.lblAFP.Text = "AFP (7.25%):";
-            // 
-            // lblISR
-            // 
-            this.lblISR.AutoSize = true;
-            this.lblISR.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblISR.Location = new System.Drawing.Point(68, 251);
-            this.lblISR.Name = "lblISR";
-            this.lblISR.Size = new System.Drawing.Size(129, 26);
-            this.lblISR.TabIndex = 7;
-            this.lblISR.Text = "ISR (Renta):";
-            // 
-            // lblTotal
-            // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(68, 280);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(217, 26);
-            this.lblTotal.TabIndex = 8;
-            this.lblTotal.Text = "Total de Deducciones:";
-            // 
-            // lblSalarioaResibir
-            // 
-            this.lblSalarioaResibir.AutoSize = true;
-            this.lblSalarioaResibir.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSalarioaResibir.Location = new System.Drawing.Point(77, 315);
-            this.lblSalarioaResibir.Name = "lblSalarioaResibir";
-            this.lblSalarioaResibir.Size = new System.Drawing.Size(171, 26);
-            this.lblSalarioaResibir.TabIndex = 9;
-            this.lblSalarioaResibir.Text = "Salario a Resibir:";
+            this.dgvAcumulado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAcumulado.Location = new System.Drawing.Point(23, 194);
+            this.dgvAcumulado.Name = "dgvAcumulado";
+            this.dgvAcumulado.RowHeadersWidth = 51;
+            this.dgvAcumulado.RowTemplate.Height = 24;
+            this.dgvAcumulado.Size = new System.Drawing.Size(479, 150);
+            this.dgvAcumulado.TabIndex = 11;
             // 
             // btnLooo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 419);
-            this.Controls.Add(this.lblSalarioaResibir);
-            this.Controls.Add(this.lblTotal);
-            this.Controls.Add(this.lblISR);
-            this.Controls.Add(this.lblAFP);
-            this.Controls.Add(this.lblISSS);
-            this.Controls.Add(this.btnLimpiar);
+            this.ClientSize = new System.Drawing.Size(565, 472);
+            this.Controls.Add(this.dgvAcumulado);
+            this.Controls.Add(this.txtMeses);
             this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.lblSueldo);
-            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.txtMontoMensual);
             this.Controls.Add(this.lblCalculo);
             this.Name = "btnLooo";
             this.Text = "Mi Pimera Aplicacion ";
+            this.Load += new System.EventHandler(this.btnLooo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtMeses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAcumulado)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,15 +106,10 @@
         #endregion
 
         private System.Windows.Forms.Label lblCalculo;
-        private System.Windows.Forms.TextBox txtSueldo;
-        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.TextBox txtMontoMensual;
         private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Button btnLimpiar;
-        private System.Windows.Forms.Label lblISSS;
-        private System.Windows.Forms.Label lblAFP;
-        private System.Windows.Forms.Label lblISR;
-        private System.Windows.Forms.Label lblTotal;
-        private System.Windows.Forms.Label lblSalarioaResibir;
+        private System.Windows.Forms.NumericUpDown txtMeses;
+        private System.Windows.Forms.DataGridView dgvAcumulado;
     }
 }
 
--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
@@ -21,57 +21,44 @@ namespace miPrimeraAplicacion
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            // 1. Validar que la entrada sea un número decimal válido
-            if (!decimal.TryParse(txtSueldo.Text, out decimal sueldo) || sueldo <= 0)
-            {
-                MessageBox.Show("Por favor ingrese un sueldo válido.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtSueldo.Focus();
-                return;
-            }
-            // 2. Descuentos básicos
-            decimal isss = Math.Min(sueldo * 0.03m, 30.00m); // ISSS 3%, máximo $30.00
-            decimal afp = sueldo * 0.0725m;                  // AFP 7.25%
+            // 1. Lectura de variables
+            int nMeses = (int)txtMeses.Value;
+            decimal montoMensual = Convert.ToDecimal(txtMontoMensual.Text);
 
-            // 3. Salario Gravable para el ISR
-            decimal gravable = sueldo - isss - afp;
-            decimal isr = 0.00m;
+            // 2. Creación de la estructura de la tabla (DataTable)
+            DataTable dt = new DataTable();
+            dt.Columns.Add("N° Mes", typeof(int));
+            dt.Columns.Add("Monto del Mes", typeof(decimal));
+            dt.Columns.Add("Acumulado", typeof(decimal));
 
-            // 4. Cálculo del ISR según tramos de ley
-            if (gravable > 2038.10m)
+            // 3. Proceso de acumulación
+            decimal acumulado = 0;
+
+            for (int i = 1; i <= nMeses; i++)
             {
-                isr = ((gravable - 2038.10m) * 0.30m) + 288.57m;
-            }
-            else if (gravable > 895.24m)
-            {
-                isr = ((gravable - 895.24m) * 0.20m) + 60.00m;
-            }
-            else if (gravable > 472.00m)
-            {
-                isr = ((gravable - 472.00m) * 0.10m) + 17.67m;
+                acumulado += montoMensual; // Suma acumulativa
+
+                // Agregar fila a la tabla
+                dt.Rows.Add(i, montoMensual, acumulado);
             }
 
-            // 5. Totales
-            decimal totalDeducciones = isss + afp + isr;
-            decimal salarioNeto = sueldo - totalDeducciones;
+            // 4. Asignar datos al DataGridView
+            dgvAcumulado.DataSource = dt;
 
-            // 6. Imprimir en pantalla con formato de moneda ($0.00)
-            lblISSS.Text = "ISSS (3%): " + isss.ToString("C2");
-            lblAFP.Text = "AFP (7.25%): " + afp.ToString("C2");
-            lblISR.Text = "ISR (Renta): " + isr.ToString("C2");
-            lblTotal.Text = "Total de Deducciones: " + totalDeducciones.ToString("C2");
-            lblSalarioaResibir.Text = "Salario a Recibir: " + salarioNeto.ToString("C2");
+            // Formato de moneda para las columnas numéricas
+            dgvAcumulado.Columns["Monto del Mes"].DefaultCellStyle.Format = "C2";
+            dgvAcumulado.Columns["Acumulado"].DefaultCellStyle.Format = "C2";
 
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
-            lblISSS.Text = "ISSS (3%):";
-            lblAFP.Text = "AFP (7.25%):";
-            lblISR.Text = "ISR (Renta):";
-            lblTotal.Text = "Total de Deducciones:";
-            lblSalarioaResibir.Text = "Salario a Recibir:";
-
             
+        }
+
+        private void btnLooo_Load(object sender, EventArgs e)
+        {
+
         }
     }
         
